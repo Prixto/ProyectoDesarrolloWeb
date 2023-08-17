99f6372db9e8cfe178bbc442c6bfbd497a32b1fc
Javi <javisdgv@gmail.com>
2023-08-17 15:06:36 -0500

    actualización de formulario

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 
-D
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
     
-    event.preventDefault();
-    
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    
-    var submissionInfo = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
-    alert("Submitted Information:\n" + submissionInfo);
-    
+        event.preventDefault();
+        
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+        
+        var submissionInfo = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+        alert("Submitted Information:\n" + submissionInfo);
+        
+    });
 });
+
